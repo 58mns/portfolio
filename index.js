d3a9f3bca83f18073aeb4dd8612abe71b12bac23
58mns <mertnurettin.mn@gmail.com>
2023-10-13 19:40:23 +0200

    added json objects for abstraction + experimenting with project elements

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function fillSkills() {
+  console.log("skills added");
+}
+
+function fillProjects() {
+  console.log("projects added");
+}
+
+window.onload = (event) => {
+  fillSkills();
+  fillProjects();
+};
