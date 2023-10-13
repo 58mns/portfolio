d76e5ea8360c4f89d1dcc275ccfc206478a7ce39
58mns <mertnurettin.mn@gmail.com>
2023-10-14 00:01:57 +0200

    reading data from json file and making elements

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-"use strict";
+import projects from "./projects.json" assert { type: "json" };
+("use strict");
 
 function fillSkills() {
   console.log("skills added");
 }
 
 function fillProjects() {
-  console.log("projects added");
+  if (projects.projects.length > 0) {
+    let innerContent = "";
+    projects.projects.forEach((project) => {
+      let content = `<div class="box">
+    <div class="image-container">
+      <img src="${project.image}" alt="${project.alt}" width="150"
+        height="100">
+    </div>
+    <div class="triangle-up"></div>
+    <div class="project-info">
+      <div class="project-title">
+        ${project.name}
+      </div>
+      <div class="project-description">
+      ${project.description}
+      </div>
+      <div class="popup-link">
+        click
+      </div>
+    </div>
+  </div> <br>`;
+      innerContent += content;
+    });
+
+    let projectsElement = document.querySelector(".projects-container");
+    projectsElement.insertAdjacentHTML("afterbegin", innerContent);
+    console.log("projects added");
+  }
 }
 
-window.onload = (event) => {
+function init() {
   fillSkills();
   fillProjects();
-};
+}
+
+window.onload = init;
