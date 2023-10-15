6c627f78ca99c64536323e834925e8f59bacf7a4
58mns <mertnurettin.mn@gmail.com>
2023-10-15 22:03:31 +0200

    css of skill elements done + started with reading json data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-"use strict";
+import skills from "./skills.json" assert { type: "json" };
+("use strict");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -10,9 +11,48 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
+function fillSkills() {
+  if (skills.skills.length > 0) {
+    let innerContent = "";
+    skills.skills.forEach((skill) => {
+      
+      console.log(skill.category);
+      skill.tools.forEach((tool) => {
+
+      });
+
+      let content = `<div class="box">
+    <div class="image-container">
+      <img src="${skill.categ}" alt="${project.alt}" width="150"
+        height="100">
+    </div>
+    <div class="triangle-up"></div>
+    <div class="project-info">
+      <div class="project-title">
+        ${project.name}
+      </div>
+      <div class="project-description">
+      ${project.description}
+      </div>
+      <div class="popup-link">
+        click
+      </div>
+    </div>
+  </div> <br>`;
+      innerContent += content;
+    });
+
+    let skillsElement = document.querySelector(".skills-container");
+    skillsElement.insertAdjacentHTML("afterbegin", innerContent);
+    console.log("skills added");
+  }
+}
+
 function init() {
   const hiddenElements = document.querySelectorAll(".hidden");
   hiddenElements.forEach((el) => observer.observe(el));
+
+  //fillSkills();
 }
 
 window.onload = init;
