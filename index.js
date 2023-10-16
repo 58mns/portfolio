76a48bdf8b4ac96b44a440859ac9c52b7215a9d4
58mns <mertnurettin.mn@gmail.com>
2023-10-16 03:18:40 +0200

    can read projects from json and display

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import skills from "./skills.json" assert { type: "json" };
+import projects from "./projects.json" assert { type: "json" };
 ("use strict");
 
 const observer = new IntersectionObserver((entries) => {
@@ -44,11 +45,51 @@ function fillSkills() {
   }
 }
 
+function fillProjects() {
+  if (projects.projects.length > 0) {
+    let innerContent1 = "";
+
+    projects.projects.forEach((project) => {
+      let content = `<div class="project-container">
+      <div class="project-image-container">
+        <img src="${project.image}" alt="${project.alt}">
+      </div>
+
+      <div class="project-description description-text-1">
+        <div class="project-title project-text-1">
+          ${project.title}
+        </div>
+
+        <div class="project-category project-text-2">
+        ${project.category}
+        </div>
+
+        <div class="project-about project-text-3">
+        ${project.about}
+        </div>
+
+        <button name="button-project" value="${project.title}" class="project-button project-text-4"
+          onclick="console.log(this.value)">
+          More info
+        </button>
+
+      </div>
+    </div>`;
+
+      innerContent1 += content;
+    });
+
+    let projectsElement = document.querySelector(".projects-container");
+    projectsElement.insertAdjacentHTML("afterbegin", innerContent1);
+  }
+}
+
 function init() {
   const hiddenElements = document.querySelectorAll(".hidden");
   hiddenElements.forEach((el) => observer.observe(el));
 
   fillSkills();
+  fillProjects();
 }
 
 window.onload = init;
